d32a6e980203822c95bada26cb0e051e1cea77e0
Andrew Madden <andrewsmadden@gmail.com>
2021-09-24 23:15:14 -0500

    Added a few crypto classes

---
+++ AndrewMadden.Trading.RobinHoodConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AndrewMadden.Trading.RobinHoodConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bruh: ");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
